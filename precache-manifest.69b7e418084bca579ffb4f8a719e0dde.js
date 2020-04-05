c314d4dd6ef114c923c2295b807afc9f81867b71
= <=>
2020-04-06 02:17:02 +0530

    Updates

---
+++ precache-manifest.69b7e418084bca579ffb4f8a719e0dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24aaff01b681555a83042e10ebfba7be",
+    "url": "/react-starter-part-1/index.html"
+  },
+  {
+    "revision": "8e5275661da816e613bc",
+    "url": "/react-starter-part-1/static/css/main.a3f6beff.chunk.css"
+  },
+  {
+    "revision": "147f1327e7798733efc4",
+    "url": "/react-starter-part-1/static/js/2.d562cfe4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-starter-part-1/static/js/2.d562cfe4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e5275661da816e613bc",
+    "url": "/react-starter-part-1/static/js/main.fb96d8c0.chunk.js"
+  },
+  {
+    "revision": "2f58992ab887cea45254",
+    "url": "/react-starter-part-1/static/js/runtime-main.dff043eb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-starter-part-1/static/media/logo.5d5d9eef.svg"
+  }
+]);
